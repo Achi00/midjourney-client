1fbb93eb713c6a6e64f056d49cad514a82dfb1f9
unknown <toolvlg@gmail.com>
2023-12-21 18:02:58 +0400

    fix and clear errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,6 +35,10 @@ const Hero = () => {
 
   const [errorMessage, setErrorMessage] = useState("");
 
+  // change prompt input stlye in case of inappropriate prompt
+  const [promptInputClass, setPromptInputClass] =
+    useState("normal-input-class");
+
   // toggle between input and microphone
   const handleToggle = () => {
     setIsUsingSpeech(!isUsingSpeech); // Toggle between input and speech-to-text
@@ -116,6 +120,8 @@ const Hero = () => {
           break;
         case "Inappropriate content detected in the prompt.":
           toast.error("Error: Inappropriate content detected in the prompt.");
+          setPromptInputClass("border-2 border-red-700");
+          setStep(2);
           break;
         default:
           console.error("Unexpected error:", errorData);
@@ -171,6 +177,8 @@ const Hero = () => {
     formData.append("email", email);
     try {
       setLoading(true);
+      setPromptInputClass("");
+      setErrorMessage("");
       const response = await fetch(
         "https://abovedigital-1696444393502.ew.r.appspot.com/generate-and-swap-face",
         {
@@ -234,30 +242,36 @@ const Hero = () => {
           </div>
         )}
         {loading && (
-          <button
-            disabled
-            type="button"
-            className="py-4 px-8 me-2 text-lg font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
-          >
-            <svg
-              aria-hidden="true"
-              role="status"
-              className="inline w-8 h-8 me-3 text-gray-200 animate-spin dark:text-gray-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <>
+            <h2 className="text-semibold w-1/4">
+              Our Artificial Intelligence System is Creating image by your
+              imagination, Please be patient, it takes about 60 seconds
+            </h2>
+            <button
+              disabled
+              type="button"
+              className="py-4 px-8 me-2 text-lg font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
             >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="#1C64F2"
-              />
-            </svg>
-            Loading...
-          </button>
+              <svg
+                aria-hidden="true"
+                role="status"
+                className="inline w-8 h-8 me-3 text-gray-200 animate-spin dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                />
+              </svg>
+              Loading...
+            </button>
+          </>
         )}
         {error && (
           <div>
@@ -308,7 +322,7 @@ const Hero = () => {
               ) : (
                 <input
                   type="text"
-                  className="bg-gray-50 border fadeIn border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block xl:w-3/4 lg:w-full md:w-full sm:full xs:w-[150%] p-2.5 outline-violet-700"
+                  className={`bg-gray-50 border fadeIn border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block xl:w-3/4 lg:w-full md:w-full sm:full xs:w-[150%] p-2.5 outline-violet-700 ${promptInputClass}`}
                   value={prompt}
                   onChange={handlePromptChange}
                   placeholder="Enter prompt"
